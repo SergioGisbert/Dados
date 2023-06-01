dba30c081e1b32e58081c503b02bef8a1b4fa4ce
SergioGisbert <128911023+SergioGisbert@users.noreply.github.com>
2023-06-01 13:48:46 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dados';
+  dad_izq = "../assets/img/dice1.png";
+  dad_der = "../assets/img/dice4.png";
+  num1: number = 1;
+  num2: number = 2;
+
+  tirarDados(): void{
+    this.num1 = Math.round(Math.random() * 5) + 1;
+    this.num2 = Math.round(Math.random() * 5) + 1;
+    this.dad_izq = "../assets/img/dice" + this.num1 + ".png";
+    this.dad_der = "../assets/img/dice" + this.num2 + ".png";
+  }
 }
